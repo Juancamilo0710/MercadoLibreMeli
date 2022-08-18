2ecad41e39b8a5e0d2992fdf92434a66a6d12c30
Juan Camilo Sanchez <juansanchez@wposs.com>
2022-08-18 12:47:30 -0500

    IMPLEMENTACIÓN - Se agregan pantallas para consumo de categorias y paises

--- app/src/main/java/com/example/mercadolibremeli/view/category/Categorias.java
+++ app/src/main/java/com/example/mercadolibremeli/view/category/Categorias.java
@@ -16,5 +16,6 @@ public class Categorias extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categorias);
     }
 }
--- app/src/main/java/com/example/mercadolibremeli/view/paises/Paises.java
+++ app/src/main/java/com/example/mercadolibremeli/view/paises/Paises.java
@@ -3,11 +3,14 @@ package com.example.mercadolibremeli.view.paises;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.mercadolibremeli.R;
+
 public class Paises extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paises);
     }
 
 
